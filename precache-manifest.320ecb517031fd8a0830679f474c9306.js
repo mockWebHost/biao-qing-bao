0c47149be689c0f86a357d39a08dc65c6583bdb3
Summer Xia <sxia@ixl.com>
2020-10-16 10:31:14 -0700

    Updates

---
+++ precache-manifest.320ecb517031fd8a0830679f474c9306.js
@@ -0,0 +1,33 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbc28e592641fb54124e4a207967fc4",
+    "url": "/biao-qing-bao/index.html"
+  },
+  {
+    "revision": "e68eac552f6cc776af20",
+    "url": "/biao-qing-bao/static/css/2.56004828.chunk.css"
+  },
+  {
+    "revision": "fe78a6de4461b33503ae",
+    "url": "/biao-qing-bao/static/css/main.9759ce58.chunk.css"
+  },
+  {
+    "revision": "e68eac552f6cc776af20",
+    "url": "/biao-qing-bao/static/js/2.05062e5f.chunk.js"
+  },
+  {
+    "revision": "098c472c2f048d48fb8c9d81c57136bd",
+    "url": "/biao-qing-bao/static/js/2.05062e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe78a6de4461b33503ae",
+    "url": "/biao-qing-bao/static/js/main.d6cc1abd.chunk.js"
+  },
+  {
+    "revision": "a152471488f1e2db1109",
+    "url": "/biao-qing-bao/static/js/runtime-main.10204441.js"
+  },
+  {
+    "url": "/biao-qing-bao/static/media/checkbcd1a436590360ed53ac2f46c76e43e4.bcd1a436.png"
+  }
+]);
